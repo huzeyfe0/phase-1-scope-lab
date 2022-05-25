03ca29637cb0e5e342709cbe6d26b9a9ecd8ccd8
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-05-25 14:20:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'asds'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(best){
+    bestCustomer = best;
+}
+
+
+
+function changeLeastFavoriteCustomer(customer){
+    leastFavoriteCustomer = customer;
+}
